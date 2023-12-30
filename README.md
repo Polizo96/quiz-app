4065fd3402b21cc629f4751c4f34feaf01b6e130
NikosPol <polizopoulos96@gmail.com>
2023-12-30 18:37:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# quiz-app
+A quiz app using react, java, spring, hibernate and mysql as a db
---
+++ quiz-app-client/quiz-app-client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => {
+	return (
+		<main>
+			<h2 className="mt-5">Welcome, Let's play a quiz!</h2>
+		</main>
+	)
+}
+
+export default Home
